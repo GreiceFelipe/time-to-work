1cace7bc381ed8eb61cdfd17925fe3d07189423a
Greice Felipe <greice.n.felipe@gmail.com>
2020-09-26 21:36:16 -0300

    Change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if User.all.blank?
+    user = User.create(email: 'text@exemple.com', password:'123456')
+
+    i = 10
+    until i < 0  do
+        w = WorkHistory.new(starts_at: Time.now-i.days)
+        w.ends_at = w.starts_at + 9.hours
+        w.user_id = user.id
+        w.save!
+        puts w.id
+        i -= 1
+    end
+end
